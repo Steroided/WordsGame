6676cc2f5a35d76abb959905b2db1ec06a17fce3
Nickita <nikoniko5120@gmail.com>
2025-04-10 15:56:01 +0300

    Сделал менеджер настроек и настройки аудио

---
+++ Assets/Scenes/Examples/OptionsManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class OptionsManager : MonoBehaviour {
+    [Header("Buttons")]
+    [SerializeField]
+    private Button _optionsButton;
+    [SerializeField]
+    private Button _soundButton;
+    [Header("Sliders")]
+    [SerializeField]
+    private Slider _musicSlider;
+    [SerializeField]
+    private Slider _soundSlider;
+    [Header("Panels")]
+    [SerializeField]
+    private GameObject _pausePanel;
+
+    public void Start()
+    {
+        Init();
+    }
+    public void Init()
+    {
+        SoundManager.PlayMusic("Swinging Pants");
+
+        _optionsButton.onClick.AddListener(() => SoundManagerButtonsProvider.PlaySound("Click1"));
+        _optionsButton.onClick.AddListener(() => TogglePause());
+        _soundButton.onClick.AddListener(() => SoundManagerButtonsProvider.ToggleMusicMuted());
+        _soundButton.onClick.AddListener(() => SoundManagerButtonsProvider.ToggleSoundMuted());
+    }
+    public void TogglePause()
+    {
+        bool needPause = Time.timeScale > 0.5f;
+        Time.timeScale = needPause ? 0 : 1;
+
+        _pausePanel.SetActive(needPause);
+    }
+
+    void OnEnable()
+    {
+        SoundManager.LoadSound("phaserUp1");
+    }
+
+    void OnDisable()
+    {
+        SoundManager.UnloadSound("phaserUp1");
+    }
+}
---
+++ Assets/Scripts/SoundManager/SoundManagerButtonsProvider.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class SoundManagerButtonsProvider
+{
+    public static void PlaySound(string name)
+    {
+        SoundManager.PlaySound(name);
+    }
+
+    public static void PlaySoundNotPausable(string name)
+    {
+        SoundManager.PlaySoundUI(name);
+    }
+
+    public static void ChangeSoundVolume(float volume)
+    {
+        SoundManager.SetSoundVolume(volume);
+    }
+
+    public static void ChangeMusicVolume(float volume)
+    {
+        SoundManager.SetMusicVolume(volume);
+    }
+
+    public static void ToggleMusicMuted()
+    {
+        SoundManager.SetMusicMuted(!SoundManager.GetMusicMuted());
+    }
+
+    public static void ToggleSoundMuted()
+    {
+        SoundManager.SetSoundMuted(!SoundManager.GetSoundMuted());
+    }
+}
